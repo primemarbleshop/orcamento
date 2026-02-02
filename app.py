347e84a4ef49854144754420b8282ffec38df426
primemarbleshop <pedroh.rios18@gmail.com>
2026-02-02 16:11:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1940,6 +1940,7 @@ def orcamentos_json():
                 'id': orcamento.id,
                 'cliente_nome': orcamento.cliente.nome,
                 'ambiente_nome': orcamento.ambiente.nome if orcamento.ambiente else 'Não definido',
+                'descricao_nome': orcamento.descricao.nome if orcamento.descricao else 'Não definid
                 'tipo_produto': orcamento.tipo_produto,
                 'material_nome': orcamento.material.nome,
                 'quantidade': orcamento.quantidade,
